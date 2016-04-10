1a504f52d26f2c4da97c749889c192e800dd4679
Ali Al Idrees <aalidrees@vmware.com>
2016-04-10 14:35:44 +0300

    Python Scripts that list NSX Logical Switches

---
+++ NSX-Logical-Switches-List.py
@@ -0,0 +1,11 @@
+import requests
+from xml.etree import ElementTree
+requests.packages.urllib3.disable_warnings()
+print 'Getting Transport Zones from NSX...'
+response = requests.get('https://10.10.11.8/api/2.0/vdn/virtualwires',verify=False,auth=('admin','VMware1!'),stream=True)
+tree = ElementTree.parse(response.raw)
+
+
+for vdnScope in tree.iter('virtualWire'):
+	print 'Name: ' + vdnScope.find ('name').text
+	print 'ID: ' + vdnScope.find('objectId').text
